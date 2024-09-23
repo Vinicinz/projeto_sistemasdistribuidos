63cc424e7dce97302fd544084da6e5e9879d17c0
Your Name <you@example.com>
2024-09-23 12:11:23 -0300

    Primeiro commit - iniciando o Spring

---
+++ src/main/java/com/projeto/projeto_sistemasdistribuidos/ProjetoSistemasdistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projeto_sistemasdistribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemasdistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemasdistribuidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/projeto_sistemasdistribuidos/ProjetoSistemasdistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projeto_sistemasdistribuidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemasdistribuidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
