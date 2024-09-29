3965e4c5a13ee2a726303b112c71761645b92972
Your Name <you@example.com>
2024-09-29 20:11:08 -0300

    Restante dos metodos CRUD na publicacao

--- src/main/java/com/projeto/projeto_sistemasdistribuidos/UsuarioService/PublicacaoService.java
+++ src/main/java/com/projeto/projeto_sistemasdistribuidos/UsuarioService/PublicacaoService.java
@@ -1,6 +1,7 @@
 package com.projeto.projeto_sistemasdistribuidos.UsuarioService;
 
 import com.projeto.projeto_sistemasdistribuidos.model.Publicacao;
+import com.projeto.projeto_sistemasdistribuidos.model.Usuario;
 import com.projeto.projeto_sistemasdistribuidos.repository.PublicacaoRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,19 @@ public class PublicacaoService {
     public List<Publicacao> listarPublicacao() {
         List<Publicacao> lista = repository.findAll();
         return lista;
+    }
+    public Publicacao criarPublicacao(Publicacao publicacao) {
+        Publicacao novaPublicacao = repository.save(publicacao);
+        return novaPublicacao;
+    }
+
+    public Publicacao editarPublicacao( Publicacao publicacao){
+        Publicacao novaPublicacao = repository.save(publicacao);
+        return novaPublicacao;
+    }
+
+    public boolean excluirPublicacao(Integer id) {
+        repository.deleteById(id);
+        return true;
     }
 }
--- src/main/java/com/projeto/projeto_sistemasdistribuidos/controller/PublicacaoController.java
+++ src/main/java/com/projeto/projeto_sistemasdistribuidos/controller/PublicacaoController.java
@@ -3,9 +3,7 @@ package com.projeto.projeto_sistemasdistribuidos.controller;
 import com.projeto.projeto_sistemasdistribuidos.UsuarioService.PublicacaoService;
 import com.projeto.projeto_sistemasdistribuidos.model.Publicacao;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,22 @@ public class PublicacaoController {
     @GetMapping
     public ResponseEntity<List<Publicacao>> listarPublicacao() {
         return ResponseEntity.status(200).body(service.listarPublicacao());
+    }
+
+    @PostMapping
+    public ResponseEntity<Publicacao> criarPublicacao(@RequestBody Publicacao publicacao) {
+        return ResponseEntity.status(201).body(service.criarPublicacao(publicacao));
+    }
+
+    @PutMapping
+    public ResponseEntity<Publicacao> editarPublicacao(@RequestBody Publicacao publicacao) {
+        return ResponseEntity.status(200).body(service.editarPublicacao(publicacao));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> excluirPublicacao(@PathVariable Integer id) {
+        service.excluirPublicacao(id);
+        return ResponseEntity.status(204).build();
     }
 
 }
