62fc38427b71d1e04f9d828df867ca1a1a98b441
Your Name <you@example.com>
2024-10-29 09:38:58 -0300

    CrossOrigin adicionado

--- src/main/java/com/projeto/projeto_sistemasdistribuidos/controller/UsuarioController.java
+++ src/main/java/com/projeto/projeto_sistemasdistribuidos/controller/UsuarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("allowedOriginPatterns")
 @RequestMapping("/usuario")
 public class UsuarioController {
 
