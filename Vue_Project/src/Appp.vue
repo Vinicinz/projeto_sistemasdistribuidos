34a9e776391e9986598e2b3be426709d2daa5ea3
hidekibtw <hidekiferreira@hotmail.com>
2024-11-03 20:25:14 -0300

    começando HTML e CSS do header da página Home

--- Vue_Project/src/Appp.vue
+++ Vue_Project/src/Appp.vue
@@ -5,10 +5,25 @@ import { RouterLink, RouterView } from 'vue-router'
 <!-- Tela pricipal com um menu de navegação e duas rotas -->
 <template>
   <header>
+    <div id="header">
+      <div class="titulo" role="header">
+        BACKDOOR
+      </div> 
+      <div class="cadastro" role="navigation">
+        <ul><li><a href="#">Entrar/Cadastrar</a></li></ul>
+      </div>
+    </div>
+
+    
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
+        <ul>
+        <li><a href=""><RouterLink to="/">Home</RouterLink></a></li>
+        <li><a href=""><RouterLink to="/login">Login</RouterLink></a></li>
+        <li><a href=""><RouterLink to="/sobreNos">Sobre Nós</RouterLink></a></li>
+        <li><a href=""><RouterLink to="/contato">Contato</RouterLink></a></li>
+      
+    </ul>
       </nav>
     </div>
   </header>
@@ -16,7 +31,6 @@ import { RouterLink, RouterView } from 'vue-router'
   <!-- RouterView que será chamado dependendo da URL -->
   <RouterView />
 </template>
-
 
 <!-- Script para iniciar o template do HomeView -->
 <script>
@@ -27,3 +41,82 @@ export default {
   }
 };
 </script>
+
+<style>
+
+#header {
+    
+    width: 800px;
+    margin-left:200px;
+    background-color: rgb(129, 74, 180);
+}
+
+.titulo {
+  font-size: 35px;
+  text-align: left;
+  margin-left: 20px;
+  padding-top: 15px;
+  font-family: fantasy;
+}
+
+.cadastro {
+  margin-right: 10px;
+  text-align: right;
+}
+
+.wrapper {
+
+    text-align: center;
+    background-color: black;
+    font-family: Arial;
+    font-size: 25px;
+    color: white;
+    margin-left:300px;
+    background-color: rgb(103, 41, 155);
+  }
+
+
+  .wrapper {
+    
+    padding: 0px;
+    margin: 0px;
+    list-style: none;
+}
+
+.wrapper ul li {
+  padding: 2px 10px;
+  display: inline;
+    
+}
+
+.wrapper ul li a{
+  padding: 2px 10px;
+  text-decoration: none;
+  color: white;
+  
+}
+
+.wrapper ul li a:hover {
+    border-bottom: 3px solid #d2bde0;
+}
+
+.cadastro ul li {
+  padding: 2px 10px;
+  display: inline;
+    
+}
+
+.cadastro ul li a{
+  padding: 2px 10px;
+  text-decoration: none;
+  color: white;
+  
+}
+
+.cadastro ul li a:hover {
+  
+    color: #bd8fe7;
+    border-bottom: 3px solid rgb(106, 49, 240);
+}
+
+</style>
