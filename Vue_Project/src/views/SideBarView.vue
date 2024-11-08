c4ccde00b2bccb2c81f48fbc716d6028ab8cb9d6
Your Name <you@example.com>
2024-11-08 15:33:56 -0300

    adição template barras laterais

---
+++ Vue_Project/src/views/SideBarView.vue
@@ -0,0 +1,26 @@
+<!-- Side bar esquerda -->
+<template>
+    <div class="sidebar">
+        <input placeholder="Pesquise em nosso site... " type="text" name="text" class="input" >
+        <h3> Menu</h3>
+        <hr>
+        <ul>
+            <li><a href="" class="active"><List /> Questões</a></li>
+            <li><a href=""><Tag /> Categorias</a></li>
+            <li><a href=""><Search/> Verificador de Links</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import List from '@/components/icons/list.vue';
+import Search from '@/components/icons/search.vue';
+import Tag from '@/components/icons/tag.vue';
+export default {
+    components: {
+        List,
+        Tag,
+        Search
+    }
+}
+</script>
