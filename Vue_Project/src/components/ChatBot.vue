71afaa485577e32677b6ad0dcf1af5e1786669f6
Your Name <you@example.com>
2024-11-22 16:44:43 -0300

    Ajuste Iframe chatbot

--- Vue_Project/src/components/ChatBot.vue
+++ Vue_Project/src/components/ChatBot.vue
@@ -1,5 +1,6 @@
 <template>
-    <iframe src='https://webchat.botframework.com/embed/ChatbotBackdoorBradesco?s=xYoXSovtpvE.Rzfj8PhW39djekMbRoCDYZCrySLus6flmS1vwnk6ujA'
-    class="chatbot-iframe"  title="Chatbot"
-    frameborder="0" style='min-width: 200px; width: 100%; min-height: 350px;'></iframe>
+    <iframe
+        src='https://webchat.botframework.com/embed/ChatbotDeployBackdoor-bot?s=cHJBqK7UwIQ.H1Sc-RoyXIyPUZbTnZ3OJzY5XmJPZs25DzAHj4S4w70'
+        class="chatbot-iframe" title="Chatbot" frameborder="0"
+        style='min-width: 200px; width: 100%; min-height: 350px;'></iframe>
 </template>
