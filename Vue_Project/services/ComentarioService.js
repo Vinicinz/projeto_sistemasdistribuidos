164b064e68c3058a1a246a66c614bd1d91f405e7
Your Name <you@example.com>
2024-11-23 17:05:01 -0300

    Ajeitando comentario services com suas rotas

--- Vue_Project/services/ComentarioService.js
+++ Vue_Project/services/ComentarioService.js
@@ -5,6 +5,20 @@ class ComentarioService {
     getComentarioCount(publicacaoId) {
         return axios.get(`http://localhost:8080/comentario/count?publicacaoId=${publicacaoId}`);
     }
+
+    getComentarios(idPublicacao) {
+        return axios.get(`http://localhost:8080/comentario/${idPublicacao}`);
+    }
+
+    enviarComentario(comentarioPayload) {
+        try {
+            const response = axios.post(`http://localhost:8080/comentario`, comentarioPayload);
+            return response.data;
+        } catch (error) {
+            console.error('Erro ao criar comentario:', error);
+            throw error;
+        }
+    }
 }
 
 
--- Vue_Project/services/PublicacaoService.js
+++ Vue_Project/services/PublicacaoService.js
@@ -20,19 +20,7 @@ class PublicacaoService {
     return axios.get(`http://localhost:8080/publicacao`);
   }
 
-  getComentarios(idPublicacao) {
-    return axios.get(`http://localhost:8080/comentario/${idPublicacao}`);
-  }
 
-  enviarComentario(comentarioPayload) {
-    try {
-      const response = axios.post(`http://localhost:8080/comentario`, comentarioPayload);
-      return response.data;
-    } catch (error) {
-      console.error('Erro ao criar comentario:', error);
-      throw error;
-    }
-  }
   getPublicacaoUsuario(userId){
     return axios.get(`http://localhost:8080/publicacao/usuario/${userId}`, userId)
   }
